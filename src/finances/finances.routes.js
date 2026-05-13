cdb043bc2e2fa4742a5f672b648422a098d13e1c
IanAlfaroSantos <ialfasa2020@gmail.com>
2026-05-13 16:54:33 -0600

    Version 25

--- src/finances/finances.routes.js
+++ src/finances/finances.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import * as financeController from './finances.controller.js'
-import { protect, restrictTo } from '../auth/auth.middleware.js'
+import { verifyAuthToken } from '../middlewares/auth.middleware.js'
+import { requireRole } from '../middlewares/role.middleware.js'
+import { USER_ROLES } from '../helpers/constants.js'
 
 const router = Router()
 
-router.use(protect)
-router.use(restrictTo('ADMIN'))
+router.use(verifyAuthToken)
+router.use(requireRole([USER_ROLES.ADMIN]))
 
 router.get('/summary', financeController.getSummary)
 
