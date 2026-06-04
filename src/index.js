98dec7dade8ffcb367e5224628b5ececca89d7a0
IanAlfaroSantos <ialfasa2020@gmail.com>
2026-06-04 10:49:25 -0600

    Ajustes: Aumentar limite de tamaño de body en Express a 15mb para imagenes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ const routes = require('./routes');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ limit: '15mb', extended: true }));
 
 connectDB();
 app.use('/api', routes);
